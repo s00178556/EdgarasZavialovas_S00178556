d86a544af104f314eaefaae529914e0ca53bc921
Edgaras Zavialovas <S00178556@mail.itsligo.ie>
2020-05-13 10:13:50 +0100

    Added classes

---
+++ EdgarasZavialovas_S00178556/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgarasZavialovas_S00178556
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+
+        public decimal price { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        public string OS_Image { get; set; }
+
+        public string Phone_Image { get; set; }
+    }
+}
